a2e8b9cc5c35adff82845332e16a192df8b73b9b
Andrey Yaromenok aka Azazello <ayaromenok@gmail.com>
2026-08-08 15:23:22 +0300

    Arduino: added RPM counter

--- esp32/rpm_conter_hw201/rpm_conter_hw201.ino
+++ esp32/rpm_conter_hw201/rpm_conter_hw201.ino
@@ -1,22 +1,30 @@
 
 // to acces to /dev/ttyUSB0 from user 'sudo usermod -a -G dialout $USER'
-#define SENSOR_PIN 15 // ESP32 pin GPIO18 connected to OUT pin of IR obstacle avoidance sensor
+#define SENSOR_PIN 15 // On DOIT ESP32  devkit V1 pin GPIO15 is close to 3V3/GND
 
+unsigned long lastflash;
+int RPM;
+void ICACHE_RAM_ATTR sens() {
+  RPM++;
+}
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  // initialize the Arduino's pin as aninput
-  pinMode(SENSOR_PIN, INPUT);
+  pinMode(SENSOR_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), sens, RISING);
 }
 
 void loop() {
-  // read the state of the the input pin:
+  
+  noInterrupts();
+  int wings= 3;  // no of wings of rotating object, for disc object use 1 with white tape on one side
+  int RPMnew = RPM/wings;  //here we used fan which has 3 wings
+  Serial.print(RPMnew);
+  Serial.print(" Rot/sec :");  //Revolutions per second
   int state = digitalRead(SENSOR_PIN);
-
-  if (state == LOW)
-    Serial.println("The obstacle is present");
-  else
-    Serial.println("The obstacle is NOT present");
-
-  delay(500);
+  Serial.print((RPMnew*60));
+  Serial.println("Rot/min. ");   //Revolutions per minute  
+  RPM=0;
+  interrupts(); 
+  delay(5000); //10 sec for manual test
 }
