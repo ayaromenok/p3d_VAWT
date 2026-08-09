675e5c4844f6c7e761c2cf970f4730fcee071f08
Andrey Yaromenok aka Azazello <ayaromenok@gmail.com>
2026-08-09 12:46:11 +0300

    up: updated vesion - code(pin #2, chassis, 2 blades for RPM counter and connector for gopro)

--- esp32/rpm_conter_hw201/rpm_conter_hw201.ino
+++ esp32/rpm_conter_hw201/rpm_conter_hw201.ino
@@ -1,27 +1,31 @@
 
 // to acces to /dev/ttyUSB0 from user 'sudo usermod -a -G dialout $USER'
-#define SENSOR_PIN 15 // On DOIT ESP32  devkit V1 pin GPIO15 is close to 3V3/GND
+// IR sensor HW-201 at #2
+// DoIt ESP32 DevKit V1
+
+#define SENSOR_PIN 2 // 
 
 unsigned long lastflash;
 int RPM;
-void ICACHE_RAM_ATTR sens() {
+void ICACHE_RAM_ATTR sensor_update() {
   RPM++;
 }
+
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   pinMode(SENSOR_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), sens, RISING);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), sensor_update, RISING);
 }
 
 void loop() {
-  
-  noInterrupts();
-  int wings= 3;  // no of wings of rotating object, for disc object use 1 with white tape on one side
+  noInterrupts();  
+  int wings= 2;  // no of wings of rotating object, for disc object use 1 with white tape on one side
   int RPMnew = RPM/wings;  //here we used fan which has 3 wings
+  Serial.print(RPM);
+  Serial.print(", ");
   Serial.print(RPMnew);
-  Serial.print(" Rot/sec :");  //Revolutions per second
-  int state = digitalRead(SENSOR_PIN);
+  Serial.print(" Rot/sec :");  //Revolutions per second  
   Serial.print((RPMnew*60));
   Serial.println("Rot/min. ");   //Revolutions per minute  
   RPM=0;
